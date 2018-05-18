00806c409344555a46bf14fb9ee29ef71e010386
Rushikesh Pupale <pupalerushikesh@gmail.com>
2018-05-18 22:00:23 +0530

    added id to spons

--- Sponsors/views.py
+++ Sponsors/views.py
@@ -5,6 +5,6 @@ from django.http import JsonResponse
 # Create your views here.
 
 def get_sponsors(request):
-	sponsors = Sponsor.objects.all().values('name','details','contact','pic','website','spons_type')
+	sponsors = Sponsor.objects.all().values()
 	sponsors_list = list(sponsors)
 	return JsonResponse(sponsors_list, safe=False)
