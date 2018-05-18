75d2f5e3fb5de0ff1ca90a0c928791da89187043
BadduCoder <sumit7engineer@gmail.com>
2018-05-19 01:20:24 +0530

    Indentation Error removed

--- App/models.py
+++ App/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class App(models.Model):
-	log = models.CharField(max_length=256)
-    url = models.URLField()
-    flag = models.BooleanField()
+	log=models.CharField(max_length=256)
+	link=models.URLField(blank=True)
+	flag=models.BooleanField()
 
 	def __str__(self):
 		return self.name
